62203564065f1bd66a83f930d5cb6b27dad0e03c
Tonaxe <ymennana@elpuig.xeill.net>
2025-12-25 08:37:11 +0100

    V02: Changed home designe

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -8,4 +8,21 @@ import { RouterModule } from '@angular/router';
   templateUrl: './home.html',
   styleUrls: ['./home.scss']
 })
-export class Home { }
+export class Home {
+  selectedImage = 'assets/1.jpeg';
+  quantity = 1;
+
+  selectImage(image: string) {
+    this.selectedImage = image;
+  }
+
+  increase() {
+    this.quantity++;
+  }
+
+  decrease() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
+}
