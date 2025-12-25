b00f4d3d6b08659201729d9a6c0f5ad44d9d3295
Tonaxe <ymennana@elpuig.xeill.net>
2025-12-25 23:46:59 +0100

    V27: Depploy4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { Home } from './pages/home/home';
 import { Checkout } from './pages/checkout/checkout';
 import { Success } from './pages/success/success';
 import { Tracking } from './pages/tracking/tracking';
+import { Cookies } from './pages/cookies/cookies';
+import { Privacy } from './pages/privacy/privacy';
+import { Terms } from './pages/terms/terms';
 
 export const routes: Routes = [
     {
@@ -20,5 +23,17 @@ export const routes: Routes = [
     {
         path: 'tracking',
         component: Tracking
+    },
+    {
+        path: 'terminos',
+        component: Terms
+    },
+    {
+        path: 'privacidad',
+        component: Privacy
+    },
+    {
+        path: 'cookies',
+        component: Cookies
     }
 ];
