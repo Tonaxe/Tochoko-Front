c5c05fdbbff2468678a1320544b689e621e78281
Tonaxe <ymennana@elpuig.xeill.net>
2026-01-05 14:09:23 +0100

    V53: Coming soon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,34 +6,38 @@ import { Tracking } from './pages/tracking/tracking';
 import { Cookies } from './pages/cookies/cookies';
 import { Privacy } from './pages/privacy/privacy';
 import { Terms } from './pages/terms/terms';
+import { DropComingSoon } from './pages/drop-coming-soon/drop-coming-soon';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: Home
-    },
-    {
-        path: 'checkout',
-        component: Checkout
-    },
-    {
-        path: 'success',
-        component: Success
-    },
-    {
-        path: 'tracking',
-        component: Tracking
-    },
-    {
-        path: 'terminos',
-        component: Terms
-    },
-    {
-        path: 'privacidad',
-        component: Privacy
-    },
-    {
-        path: 'cookies',
-        component: Cookies
-    }
+    // {
+    //     path: '',
+    //     component: Home
+    // },
+    // {
+    //     path: 'checkout',
+    //     component: Checkout
+    // },
+    // {
+    //     path: 'success',
+    //     component: Success
+    // },
+    // {
+    //     path: 'tracking',
+    //     component: Tracking
+    // },
+    // {
+    //     path: 'terminos',
+    //     component: Terms
+    // },
+    // {
+    //     path: 'privacidad',
+    //     component: Privacy
+    // },
+    // {
+    //     path: 'cookies',
+    //     component: Cookies
+    // }
+
+    { path: '', component: DropComingSoon },
+    { path: '**', redirectTo: '' }
 ];
---
+++ src/app/pages/drop-coming-soon/drop-coming-soon.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drop-coming-soon',
+  imports: [],
+  templateUrl: './drop-coming-soon.html',
+  styleUrl: './drop-coming-soon.scss',
+})
+export class DropComingSoon {
+
+}
