8318fec6b95ee337adeed2228ae93cd3e94a7c58
Tonaxe <ymennana@elpuig.xeill.net>
2026-02-15 17:52:02 +0100

    V104: aAAaaaaasdsdasddaasdasdasasddasdaas

--- src/app/pages/terms/terms.ts
+++ src/app/pages/terms/terms.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-terms',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './terms.html',
   styleUrl: './terms.scss',
 })
